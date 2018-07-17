41e473af1eb8985cfee920d2aee553565ea397a2
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-16 19:33:43 -0700

    completes #10

--- js/index.js
+++ js/index.js
@@ -15,3 +15,9 @@ $("#both").on('click', function(){
 $("#fade").on('click', function(){
   $("#asta").toggleClass("fade")
 })
+
+$('#asta').hover(function(){
+    $(this).attr("src",'https://www.w3schools.com/w3css/img_lights.jpg');
+    }, function(){
+    $(this).attr("src", 'https://vignette.wikia.nocookie.net/blackclover/images/9/98/Asta_profile.png/revision/latest?cb=20150719164752');
+})
